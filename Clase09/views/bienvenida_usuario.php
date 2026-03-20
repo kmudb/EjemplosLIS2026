f325fdc65cd229f33497490a393b6983ad8fa202
kmudb <47783830+kmudb@users.noreply.github.com>
2026-03-20 14:55:43 -0600

    cambio color de boton

--- Clase09/views/bienvenida_usuario.php
+++ Clase09/views/bienvenida_usuario.php
@@ -14,7 +14,7 @@ $controller->verificarSesion("usuario");
     <div class="card shadow text-center">
         <div class="card-body">
             <h2>Bienvenido <?php echo $_SESSION["usuario"]; ?> 👋</h2>
-            <a href="../logout.php" class="btn btn-danger mt-3">Cerrar sesión</a>
+            <a href="../logout.php" class="btn btn-primary mt-3">Cerrar sesión</a>
         </div>
     </div>
 </div>
